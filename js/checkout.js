35bf2b345e09cdd2df89fab356dafd6e86bc4d12
rwitam-b <mailtorwitam@gmail.com>
2018-09-10 01:01:09 +0530

    Checkout page changes

--- js/checkout.js
+++ js/checkout.js
@@ -4,11 +4,32 @@
     var cart_data = window.localStorage.cart_data;
 
     // Payment mode selection check/uncheck operation
-    $(".card-header").click(function () {   
+    $(".card-header").click(function () {
         $(".card-header").find("i").removeClass("fa-check-circle");
         $(".card-header").find("i").addClass("fa-circle-o");
         $(this).find("i").toggleClass("fa-circle-o");
         $(this).find("i").toggleClass("fa-check-circle")
+    });
+
+    $("#town").change(function () {
+        var town = $("#town").val();
+        var shippingCharge = 0;
+        if (town && town == "o") {
+            $("#otherArea").removeClass("no-display");
+            $("#otherTown").focus();
+            shippingCharge = 50;
+        } else {
+            $("#otherArea").addClass("no-display");
+            $("#otherTown").val("");
+            if (town == "ip" || town == "ptf" || town == "bp" || town == "nh") {
+                shippingCharge = 20;
+            }
+        }
+        if (shippingCharge == 0) {
+            $("#shippingCharge").text("Free");
+        } else {
+            $("#shippingCharge").text("₹" + shippingCharge);
+        }
     });
 
     if (cart_data) {
@@ -19,7 +40,8 @@
         var orderDetails = '<li><span>Product</span> <span>Total</span></li>';
         Object.keys(cart_data).forEach(function (i) {
             var cartObject = cart_data[i];
-            orderDetails += '<li><span>' + cartObject.productName + '(' + cartObject.brandName + ') - ' + cartObject.color + ' - ' + cartObject.size + '</span> <span>₹' + cartObject.price + '</span></li>';
+            orderDetails += '<li><span>' + cartObject.productName + '(' + cartObject.brandName + ') - ' + cartObject.color + ' - ' + cartObject.size + '</span>' +
+                ' <span>₹' + cartObject.price + ' x' + cartObject.count + '</span></li>';
         });
 
         orderDetails += '<li><span>Subtotal</span> <span>₹' + globalCartData.cartValue + '</span></li>';
